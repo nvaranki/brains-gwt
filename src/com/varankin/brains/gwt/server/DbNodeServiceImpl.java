8a9b30db0649497cd469321ec0a7e628261a5fa0
Nikolai Varankine <nvaranki@tut.by>
2023-01-16 23:50:04 +0300

    Thinker database attached to servlet

--- src/com/varankin/brains/gwt/server/DbNodeServiceImpl.java
+++ src/com/varankin/brains/gwt/server/DbNodeServiceImpl.java
@@ -1,9 +1,24 @@
 package com.varankin.brains.gwt.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.varankin.brains.db.Коллекция;
+import com.varankin.brains.db.Транзакция;
+import com.varankin.brains.db.neo4j.local.NeoАрхив;
+import com.varankin.brains.db.type.DbАрхив;
+import com.varankin.brains.db.type.DbАтрибутный;
 import com.varankin.brains.gwt.client.model.DbNode;
 import com.varankin.brains.gwt.client.service.db.DbNodeService;
 import com.varankin.brains.gwt.shared.FieldVerifier;
+import com.varankin.characteristic.Именованный;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * The server-side implementation of the RPC service.
@@ -12,6 +27,30 @@ public class DbNodeServiceImpl
         extends RemoteServiceServlet 
         implements DbNodeService 
 {
+    private final Collection<NeoАрхив> archives;
+
+    public DbNodeServiceImpl()
+    {
+        archives = new LinkedList<>();
+        try
+        {
+            archives.add( new NeoАрхив(
+                    new File( "C:\\Users\\nvara\\Projects\\Thinker\\mind-sample\\neo4j\\repo" ), //TODO DEBUG
+                    new HashMap<>() ) );
+        }
+        catch( Exception ex )
+        {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void destroy()
+    {
+        archives.forEach( NeoАрхив::закрыть );
+        archives.clear();
+        super.destroy();
+    }
 
   public String greetServer(String input) throws IllegalArgumentException {
     // Verify that the input is valid. 
@@ -47,25 +86,117 @@ public class DbNodeServiceImpl
     return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
         ">", "&gt;");
   }
-
+        
     @Override
     public DbNode[] nodesFrom( DbNode[] path ) throws IllegalArgumentException
     {
-        // lookup DB service
-        // locate node by path
-        // return combined children
-        //TODO NOT IMPL
-        return path.length == 0 ? 
-                new DbNode[]
-                { 
-                    //new DbNode( "Archive AAA", "#archive", "brains" ) 
-                    new DbNode( "Archive-s #1 TODO", "#archive", "brains" ),
-                    new DbNode( "Archive-s #2 TODO", "#archive", "brains" )
-                } : 
-                new DbNode[] 
-                { 
-                    new DbNode( "Child #1 of TODO " + path[path.length-1].name(), "#element", "brains" ),
-                    new DbNode( "Child #2 of TODO " + path[path.length-1].name(), "#element", "brains" )
-                };
+        List<DbNode> result = new LinkedList<>();
+        
+        if( path == null || path.length == 0 )
+        {
+            for( DbАрхив архив : archives )
+                try( Транзакция транзакция = архив.транзакция() )
+                {
+                    транзакция.завершить( result.add( instanceOfDbNode( архив ) ) );
+                }
+                catch( Exception ex )
+                {
+                    ex.printStackTrace();
+                }
+        }
+        else
+        {
+            DbАтрибутный target = null;
+            for( DbNode dbn : path )
+            {
+                Collection<? extends DbАтрибутный> candidates = target == null ? archives : allChildren( target );
+                target = null;
+                for( DbАтрибутный dba : candidates )
+                    try( Транзакция транзакция = dba.транзакция() )
+                    {
+                        if( equals( dba, dbn ) )
+                        {
+                            target = dba;
+                            break;
+                        }
+                        транзакция.завершить( true );
+                    }
+                    catch( Exception ex )
+                    {
+                        ex.printStackTrace();
+                        break;
+                    }
+                if( target == null ) break;
+            }
+            // extract target's collections
+            for( DbАтрибутный dba : allChildren( target ) )
+                try( Транзакция транзакция = dba.транзакция() )
+                {
+                    транзакция.завершить( result.add( instanceOfDbNode( dba ) ) );
+                }
+                catch( Exception ex )
+                {
+                    ex.printStackTrace();
+                    break;
+                }
+        }
+        return result.toArray( DbNode[]::new );
     }
+    
+    private static Collection<DbАтрибутный> allChildren( DbАтрибутный target ) 
+    {
+        if( target == null ) 
+        {
+            System.err.println( "No match of UI to DB" );
+            return Collections.emptyList();
+        }
+        else
+            try( Транзакция транзакция = target.транзакция() )
+            {
+                Collection<DbАтрибутный> result  = new LinkedList<>();
+                for( Method m : target.getClass().getMethods() )
+                    if( Modifier.isPublic( m.getModifiers() ) 
+                        && ! Modifier.isStatic( m.getModifiers() ) 
+                        && Коллекция.class.isAssignableFrom( m.getReturnType() )
+                        && m.getParameterCount() == 0 )
+                {
+                    m.setAccessible( true );
+                    result.addAll( Collection.class.cast( m.invoke( target ) ) );
+                }
+                транзакция.завершить( true );
+                return result;
+            }
+            catch( Exception ex )
+            {
+                ex.printStackTrace();
+                return Collections.emptyList();
+            }
+    }
+
+    private static DbNode instanceOfDbNode( DbАтрибутный dba )
+    {
+        return new DbNode(
+            name( dba ), 
+            dba.тип().НАЗВАНИЕ, 
+            Objects.requireNonNullElse( dba.тип().ЗОНА, "" ) );
+    }
+    
+    private static boolean equals( DbАтрибутный dba, DbNode dbn )
+    {
+        String name = name( dba );
+        String type = dba.тип().НАЗВАНИЕ;
+        String zone = Objects.requireNonNullElse( dba.тип().ЗОНА, "" );
+        //System.out.printf( "%s <> %s, %s <> %s, %s <> %s \n", dbn.name(), name, dbn.type(), type, dbn.zone(), zone );
+        return Objects.equals( dbn.name(), name ) 
+            && Objects.equals( dbn.type(), type ) 
+            && Objects.equals( dbn.zone(), zone );
+    }
+    
+    private static String name( DbАтрибутный dba )
+    {
+        return dba instanceof Именованный ? 
+            Objects.requireNonNullElse( ( (Именованный) dba ).название(), dba.тип().НАЗВАНИЕ ) : 
+            dba.тип().НАЗВАНИЕ;
+    }
+
 }
